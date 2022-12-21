54d2f3985c71aef74e31869fca7c8d473dd61866
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-12-20 20:29:38 -0800

    working on code

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+
+// After repeatedly working with the demo code and the demo Julian shared, I am still unable to get it to run properly. I have gotten to a point where I may have made it worse
+
+
 console.log('choose an image');
 
 const { S3,
@@ -13,14 +18,14 @@ exports.handler = async (event, context) => {
   // const filename = event.Records[0].s3.object.key;
 
   const bucket = 'rcimages';
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const key = event.Records[0].s3.object.key.replace(/\+/g, ' ');
   const params = {
     Bucket: bucket,
     Key: key,
   };
   let json;
   try {
-    const data = await client.send(new GetObjectCommand({
+    const data = await json.send(new GetObjectCommand({
       Bucket: bucket,
       Key: 'images.json',
     }));
@@ -31,7 +36,7 @@ exports.handler = async (event, context) => {
     json = [];
   }
   try {
-    const response = await client.send(new GetObjectCommand(params));
+    const response = await json.send(new GetObjectCommand(params));
     const imageData = {
       name: key,
       size: response.ContentLength,
@@ -44,7 +49,7 @@ exports.handler = async (event, context) => {
       json.push(imageData);
 
     }
-    await client.send(new PutObjectCommand({
+    await json.send(new PutObjectCommand({
       Bucket: bucket,
       Key: 'images.json',
       Body: JSON.stringify(json),
